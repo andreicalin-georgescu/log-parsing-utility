465a106a86610076b987f614d63686c5b286de81
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2025-08-27 12:08:23 +0300

    feat(parser): Add report generation capabilities and refactor job generation to use a stack

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@
 Simple parser for log files.
 Contains two main functions: parse_log_file and generate_report.
 
-parse_log_file: receives a log file path and returns a dictionary of log entries.
+parse_log_file: receives a log file path and returns a list of jobs.
 The log entries are objects containing the following attributes:
 - job description
 - job pid
@@ -10,7 +10,7 @@ The log entries are objects containing the following attributes:
 - end time
 - duration
 
-generate_report: receives a dictionary of log entries and generates a report.
+generate_report: receives a list of jobs and generates a report.
 The report will contain:
 - a warning if any job exceeds 5 minutes
 - an error if any job exceeds 10 minutes
@@ -28,36 +28,47 @@ ERROR_THRESHOLD = timedelta(minutes=10)
 
 def parse_log_file(filename):
     jobs = {}
-    outlier_jobs = []
+    result_jobs = []
 
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if len(row) != 4:
+
                 # determine action for malformed rows.
                 print(f"Skipping malformed row: {row}")
                 continue
             job_timestamp_string, job_description, job_status, job_pid = [item.strip() for item in row]
             job_timestamp = datetime.strptime(job_timestamp_string, TIME_FORMAT)
 
-            # create dictionary entry on START log lines
+            # add dictionary entry on START log lines with the timestamp value
             if job_status == "START":
-                jobs[job_pid] = {
-                    "job_description": job_description,
-                    "job_pid": job_pid,
-                    "start_time": job_timestamp,
-                    "end_time": None,
-                    "duration": None
-                }
-            # update dictionary entry on END log lines
-            elif job_status == "END" and job_pid in jobs:
-                jobs[job_pid]["end_time"] = job_timestamp
-                jobs[job_pid]["duration"] = job_timestamp - jobs[job_pid]["start_time"]
+                jobs[job_pid] = job_timestamp
 
-        # intermediary step to check job dictionary builds correctly
-        return jobs
+            # on END log lines, pop the timestamp value from the job_pid key
+            elif job_status == "END":
+                start_time = jobs.pop(job_pid, None)
+                if start_time:
+                    job_duration = job_timestamp - start_time
+                    result_jobs.append({
+                    "description": job_description,
+                    "pid": job_pid,
+                    "start_time": start_time.time(),
+                    "end_time": job_timestamp.time(),
+                    "duration": job_duration
+                })
+    return result_jobs
+
+def generate_report(jobs):
+    for job in jobs:
+        duration = job["duration"]
+        job_info = f"{job['description']} (PID {job['pid']}) from {job['start_time']} to {job['end_time']} - Duration: {duration}"
+        if duration > ERROR_THRESHOLD:
+            print(f"ERROR: {job_info}")
+        elif duration > WARNING_THRESHOLD:
+            print(f"WARNING: {job_info}")
 
 if __name__ == "__main__":
-    jobs = parse_log_file(LOG_FILE)
-    print(jobs)
+    result_jobs = parse_log_file(LOG_FILE)
+    generate_report(result_jobs)
 
